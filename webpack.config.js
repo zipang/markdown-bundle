5671d20e4cfe65734b80a9a4c88f2dd0ee1c65c2
zipang <christophe.desguez@gmail.com>
2017-09-05 00:56:50 +0200

    Settings and configuration

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path"),
+	minified = (process.argv.indexOf('-p') !== -1), // -p is the webpack option for minification
+	WebpackAutoInject = require("webpack-auto-inject-version");
+
+console.log("Building " + (minified ? "minified" : "full") + " version of markdown renderer");
+
+module.exports = {
+	entry: "./index.js",
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: "markdown-bundle" + (minified ? ".min" : "") + ".js"
+	},
+	externals: 'fs', // in order to make mermaid work
+	plugins: [
+		new WebpackAutoInject({
+			components: {
+				AutoIncreaseVersion: true,
+				InjectAsComment: true,
+				InjectByTag: false
+			},
+			componentsOptions: {
+				AutoIncreaseVersion: {
+					runInWatchMode: false // it will increase version with every single build!
+				},
+				InjectAsComment: {
+					tag: "markdown-bundle, build {version} - {date}"
+				}
+			}
+		})
+	]
+};
